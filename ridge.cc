d45e58093a4aaf905b4bc72c08ff4c9ddd1de265
Luciano Arellano <larebert@gmail.com>
2019-09-05 17:23:58 -0400

    added single ridge-like plot function, along with projections, also added eta (pseudorapidity) plots

--- ridge.cc
+++ ridge.cc
@@ -7,6 +7,9 @@
 #include "TDatabasePDG.h"
 
 TString out_path = "/home/luciano/Physics/CLAS/pion_ridge/";
+bool m_debug = false;
+bool m_simulation = true;
+TDatabasePDG db;
 
 void virtualframe(TLorentzVector * v1,
 		  double angle1, double angle2, double boost)
@@ -30,9 +33,55 @@ void plot(TGraph *gr, TString title)
   //cout <<  gr->GetCorrelationFactor() << endl;
 }
 
+void ridge_plot(TH2 *h2, TString out_name)
+{
+  TCanvas *c1 = new TCanvas("c1","mi cambas",1000,1000);
+  c1->SetLeftMargin(0.14);
+  h2->Draw("surf1");
+  
+  // aesthetics
+  gStyle->SetOptStat(0);
+  double theta = -30;
+  double phi   = 60;
+  h2->GetXaxis()->CenterTitle(true);
+  h2->GetXaxis()->SetTitleOffset(1.5);
+  h2->GetYaxis()->CenterTitle(true);
+
+  // main 2d plot 
+  TString out_filename = out_path+out_name;
+  c1->SaveAs(out_filename);
+
+  // mountain side plots
+  gPad->GetView()->RotateView(0,90);
+  out_filename = out_path+"mountainsideY_"+out_name;
+  c1->SaveAs(out_filename);
+  gPad->GetView()->RotateView(-90,90);
+  out_filename = out_path+"mountainsideX_"+out_name;
+  c1->SaveAs(out_filename);
+
+  // projection plots
+  TH1 * projx = h2->ProjectionX();
+  projx->SetFillColor(kBlue);
+  projx->Draw();
+  out_filename = out_path+"projx_"+out_name;
+  c1->SaveAs(out_filename);
+  
+  TH1 * projy = h2->ProjectionY();
+  projy->SetFillColor(kBlue);
+  projy->Draw();
+  out_filename = out_path+"projy_"+out_name;
+  c1->SaveAs(out_filename);
+
+  // comment next line if not running on batch mode I guess
+  delete c1;
+  delete projx;
+  delete projy;
+}
+
+// plots two th2 side by side
 void side_by_side(TH2 *hlab,TH2 *hvir, TString out_filename)
 {
-  TCanvas *c1 = new TCanvas("c1","mi cambas",1200,500);
+  TCanvas *c1 = new TCanvas("c1","mi cambas",2400,1000);
   TString corr;
   c1->SetFixedAspectRatio();
   c1->Divide(2,0,0.02,0.02);
@@ -41,22 +90,14 @@ void side_by_side(TH2 *hlab,TH2 *hvir, TString out_filename)
   
   hlab->Draw("surf1");
   gStyle->SetOptStat(0);
-  /*
-  TPaveText pt(0.8,1.5,1.7,1.8,"TR");
-  corr = Form("%.4f",hlab->GetCorrelationFactor());
-  corr = "Correlation = "+corr;
-  pt.AddText(corr);  
-  pt.Draw();*/
+  // TODO: maybe add text with entry number
   double theta = -30;
   double phi   = 60;
   hlab->GetXaxis()->CenterTitle(true);
   hlab->GetXaxis()->SetTitleOffset(1.5);
   hlab->GetYaxis()->CenterTitle(true);
   gPad->GetView()->RotateView(theta,phi);  
-  //gPad->Modified();
-  //gPad->Update();
   
-
   c1->cd(2);
   hvir->Draw("surf1");
   gStyle->SetOptStat(0);
@@ -64,24 +105,20 @@ void side_by_side(TH2 *hlab,TH2 *hvir, TString out_filename)
   hvir->GetXaxis()->SetTitleOffset(1.5);
   hvir->GetYaxis()->CenterTitle(true);
   gPad->GetView()->RotateView(-30,60);  
-  /*
-  corr = Form("%.4f",hvir->GetCorrelationFactor());
-  corr = "Correlation = "+corr;
-  pt.AddText(corr);
-  pt.Draw();*/
 
-  
   out_filename = out_path+out_filename;
   c1->SaveAs(out_filename);
-  //delete c1;
+  delete c1;
 }
 
 void ridge()
 {
-  TDatabasePDG db;
-  bool m_debug = false;
-  bool m_simulation = true;
-
+  if ( m_simulation == true ){
+    out_path+="simulation/";
+  } else {
+    out_path += "data/";
+  }
+  
   TFile *f;
   Double_t kEbeam;
   if ( m_simulation ){
@@ -131,13 +168,18 @@ void ridge()
 		       "#gamma^{*}-P frame;p_{y}(#pi^{+});p_{y}(#pi^{-})",
 		       30,-1.5,1.5, 30,-1.5,1.5);
 
-  TH2 * ns = new TH2F("Signal Distribution, lab frame",
-		      "N_{s}(#Delta#phi,#Delta#eta);#Delta#phi;#Delta#theta",
+  TH2 * ns = new TH2F("Signal Distribution, lab frame (theta)",
+		      "N_{s}(#Delta#phi, #Delta#theta);#Delta#theta;#Delta#phi",
 		      16,-4.0,4.0,
 		      16,-4.0,4.0);
+
+  TH2 * ns_eta = new TH2F("Signal Distribution, lab frame",
+			  "N_{s}(#Delta#phi, #Delta#eta);#Delta#eta;#Delta#phi",
+			  16,-4.0,4.0,
+			  16,-4.0,4.0);
   
   TH2 * ns_vir = new TH2F("Signal Distribution, #gamma^{*} frame",
-			  "N_{s}(#Delta#phi,#Delta#eta), #gamma^{*} frame;#Delta#phi;#Delta#theta",
+			  "N_{s}(#Delta#phi, #Delta#theta), #gamma^{*} frame;#Delta#theta;#Delta#phi",
 			  16,-4.0,4.0,
 			  16,-4.0,4.0);
   
@@ -178,17 +220,20 @@ void ridge()
       TVector3 piplus (px[i],py[i],pz[i]);
       Float_t phiplus    = piplus.Phi();
       Float_t thetaplus  = piplus.Theta();
+      Float_t etaplus    = piplus.Eta();
       //for ( int j = 0; j < neg_pions.size(); j++ ) {
       for ( auto j: neg_pions ) {
 	TVector3 piminus (px[j],py[j],pz[j]);
 	Float_t phiminus    = piminus.Phi();
 	Float_t thetaminus  = piminus.Theta();
-	ns->Fill(phiplus-phiminus,thetaplus-thetaminus);
-	if ( events < 1000 )
-	  cout << phiplus << " " << phiminus << endl;
+	Float_t etaminus    = piminus.Eta();
+	ns->Fill(thetaplus-thetaminus,phiplus-phiminus);
+	ns_eta->Fill(etaplus-etaminus,phiplus-phiminus);
       }
     }
 
+    // this analysis doesn't use the virtual photon frame
+    /*
     // virtual photon frame is per event
     Double_t scattered_energy = sqrt((*epx)*(*epx)+(*epy)*(*epy)+(*epz)*(*epz));
     if ( scattered_energy < 0.005 )
@@ -243,7 +288,7 @@ void ridge()
 			   Particles[neg_pions[j]].Pz());
 	Float_t phiminus    = piminus.Phi();
 	Float_t thetaminus  = piminus.Theta();
-	ns_vir->Fill(phiplus-phiminus,thetaplus-thetaminus);
+	ns_vir->Fill(thetaplus-thetaminus,phiplus-phiminus);
       }
     }
     
@@ -255,12 +300,19 @@ void ridge()
     
     //cout << pos_pions.size() << " " << neg_pions.size() << endl;;
     counter++;
-    
+    */
   }
 
   cout << "ran through this many events: " << events << endl;
   cout << "processed this many events: " << counter << endl;
   
-  side_by_side(ns,ns_vir,"NS_lab_virtual.png");
+  //side_by_side(ns,ns_vir,"NS_lab_virtual.png");
+
+  ridge_plot(ns,"single_ridge_theta.pdf");
+  ridge_plot(ns_eta,"single_ridge_eta.pdf");
+
+  ridge_plot(ns,"single_ridge_theta.png");
+  ridge_plot(ns_eta,"single_ridge_eta.png");
+    
   cout << "finished correctly" << endl;
 }
